ecd4a92d196a2c804d270363f172d5e6d9df5740
Zabai Armas Herrera <zbrmas@hotmail.com>
2022-06-14 22:02:56 +0100

    feat: filters hook

---
+++ hooks/useFilters/index.ts
@@ -0,0 +1,33 @@
+import {
+  getCategoryIdsFromQueryString,
+  sanetizeCategoriesIds,
+} from "@/utils/categoryUtils";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function useFilters() {
+  const { isReady, query } = useRouter();
+  const [categories, setCategories] = useState<Array<number>>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    if (isReady) {
+      query.categories &&
+        setCategories(
+          sanetizeCategoriesIds(
+            getCategoryIdsFromQueryString(query.categories.toString())
+          )
+        );
+      setSearchTerm(query.term?.toString() ?? searchTerm);
+    }
+  }, [isReady]);
+
+  const toggleCategory = (categoryId: number) =>
+    setCategories(
+      categories.includes(categoryId)
+        ? categories.filter((categoryIdx) => categoryIdx !== categoryId)
+        : [...categories, categoryId]
+    );
+
+  return { categories, searchTerm, setSearchTerm, toggleCategory };
+}
