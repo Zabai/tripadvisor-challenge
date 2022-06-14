772885a86402303d75500a9b3ab550bfc4b276b9
Zabai Armas Herrera <zbrmas@hotmail.com>
2022-06-14 22:23:31 +0100

    ci: added image domains to next js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["fastly.4sqi.net", "ss3.4sqi.net"],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
