5928d38936561e41a49d5134515e4c98ddc63f5c
Zabai Armas Herrera <zbrmas@hotmail.com>
2022-06-14 22:30:09 +0100

    feat: prefetching place search

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,37 @@
+import { getPlacesByLocationAndCategory } from "@/api/foursquare/places";
 import Header from "@/components/header";
 import Layout from "@/components/layout";
+import FOURSQUARE_PLACES from "@/constants/api/foursquare/places";
 import PlaceList from "@/features/search/placeList";
 import Searcher from "@/features/search/searcher";
+import {
+  getCategoryIdsFromQueryString,
+  sanetizeCategoriesIds,
+} from "@/utils/categoryUtils";
 import { Container } from "@mui/material";
-import type { NextPage } from "next";
+import type { GetServerSidePropsContext, NextPage } from "next";
+import { dehydrate, QueryClient } from "react-query";
+
+export async function getServerSideProps({
+  query: { categories = "", term },
+}: GetServerSidePropsContext) {
+  const queryClient = new QueryClient();
+
+  if (term) {
+    await queryClient.prefetchQuery(
+      FOURSQUARE_PLACES.QUERIES.PLACE_SEARCH,
+      () =>
+        getPlacesByLocationAndCategory(
+          term.toString(),
+          sanetizeCategoriesIds(
+            getCategoryIdsFromQueryString(categories.toString())
+          )
+        )
+    );
+  }
+
+  return { props: { dehydratedState: dehydrate(queryClient) } };
+}
 
 const Home: NextPage = () => {
   return (
