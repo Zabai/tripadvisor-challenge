634640cd74ba69437aca98b4d9dd9458d85c450f
Zabai Armas Herrera <zbrmas@hotmail.com>
2022-06-14 21:24:20 +0100

    feat: header component

---
+++ components/header/index.tsx
@@ -0,0 +1,30 @@
+import APP from "@/constants/app";
+import { Stack, Typography } from "@mui/material";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <Stack alignItems="center" mt="2rem">
+      <Link href="/">
+        <Typography
+          component="div"
+          gutterBottom
+          sx={{
+            borderBottom: "2px solid",
+            borderTop: "2px solid",
+            color: "primary.main",
+            cursor: "pointer",
+            fontFamily: "oswald",
+            letterSpacing: ".15em",
+            textAlign: "center",
+            textTransform: "uppercase",
+            userSelect: "none",
+          }}
+          variant="h2"
+        >
+          {APP.NAME}
+        </Typography>
+      </Link>
+    </Stack>
+  );
+}
